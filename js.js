42c852f10466b6d81c83553421b6d856401a6dc7
Ótott-Kovács Tamás <szt2otokov@vasvari.org>
2024-02-20 12:28:28 +0100

    nothing really happened

--- js.js
+++ js.js
@@ -11,11 +11,15 @@ window.onload = () => {
     for(box of boxes.values()){
         boxList.push(box)
     }
+    const root = $(":root")[0]
     boxList.forEach(b => {
-        b.style.add("")
+        b.style = "";
         b.onclick = () => {
             if(!b.classList.contains("gap")) {
-                
+                let gp = $(".gap")[0]
+                gp.style.setProperty("grid-row",root.style.getPropertyValue(`--${b.id}-row`))
+                gp.style.setProperty("grid-column",root.style.getPropertyValue(`--${b.id}-col`))
+                console.log("gap is at " + gp.style.getPropertyValue("grid-row").toString() + ", " + gp.style.getPropertyValue("grid-column").toString())
             }
         }
     })
