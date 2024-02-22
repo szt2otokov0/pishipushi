64d5e4d4046e41b3250be5c64bfebc769d4b9b24
Ótott-Kovács Tamás <szt2otokov@vasvari.org>
2024-02-22 09:40:19 +0100

    boxes move somewhere

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
-function $(query){
-    if(query[0] != '#' ) return document.querySelectorAll(query)
+function $(query) {
+    if (query[0] != '#') return document.querySelectorAll(query)
     return document.querySelector(query);
 }
 
@@ -8,18 +8,23 @@ function $(query){
 window.onload = () => {
     let boxes = $(".box")
     let boxList = [];
-    for(box of boxes.values()){
-        boxList.push(box)
+    for (box of boxes.values()) {
+        let randIndex = Math.random * boxes.length;
+        boxList.push(boxes[randIndex])
+        boxes.splice(boxes.indexOf(box),1)
     }
-    const root = $(":root")[0]
+    const root = $(":root")[0];
+    const rootStyle = getComputedStyle(root);
     boxList.forEach(b => {
-        b.style = "";
         b.onclick = () => {
-            if(!b.classList.contains("gap")) {
+            if (!b.classList.contains("gap")) {
                 let gp = $(".gap")[0]
-                gp.style.setProperty("grid-row",root.style.getPropertyValue(`--${b.id}-row`))
-                gp.style.setProperty("grid-column",root.style.getPropertyValue(`--${b.id}-col`))
-                console.log("gap is at " + gp.style.getPropertyValue("grid-row").toString() + ", " + gp.style.getPropertyValue("grid-column").toString())
+                gp.style.setProperty("grid-area", `${rootStyle.getPropertyValue(`--${b.id}-row`)} /
+                 ${rootStyle.getPropertyValue(`--${b.id}-col`)}`)
+                const location = getComputedStyle(b).getPropertyValue("grid-row")
+                console.log(location)
+                root.style.setProperty(`--${b.id}-row`,location[0])
+                root.style.setProperty(`--${b.id}-col`,location[1])
             }
         }
     })
